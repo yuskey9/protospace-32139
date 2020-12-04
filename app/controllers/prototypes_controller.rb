3a4ebd2e4c8b66cf8d69c7237365e88304a3233d
yuskey9 <yuskey0130@gmail.com>
2020-12-04 12:49:32 +0900

    挙動確認修正しました（２回目

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,17 +1,15 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
-  before_action :move_to_top_page, only: :edit
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_top_page, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
   end
 
-
   def new
     @prototype = Prototype.new
   end
-
 
   def create
     @prototype = Prototype.new(prototype_params)
@@ -22,54 +20,41 @@ class PrototypesController < ApplicationController
     end
   end
 
-
   def show
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
-
   def edit
   end
 
-
-
   def update
-    if prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to prototype_path(prototype)
     else
       render :edit
     end
   end
 
-
-
   def destroy
-    if prototype.destroy
-      redirect_to prototypes_path
+    if @prototype.destroy
+      redirect_to root_path
     else
       redirect_to root_path
     end
   end
 
-
   private
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-    
-  end
 
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)    
+  end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
 
-
   def move_to_top_page
-    unless current_user == @prototype.user
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
-
-
 end
